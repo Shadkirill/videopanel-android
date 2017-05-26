917c2efe8b7de249098c309f6b5a3b6c49219a8b
d-fedorenko-dev <d-fedorenko-dev@gmail.com>
2017-05-26 13:20:39 +0300

    v0.2 - fullscreen show

--- app/src/main/java/ru/com/videopanel/activities/ShowActivity.java
+++ app/src/main/java/ru/com/videopanel/activities/ShowActivity.java
@@ -9,6 +9,8 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -65,6 +67,10 @@ public class ShowActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_show);
 
         getSupportActionBar().hide();
